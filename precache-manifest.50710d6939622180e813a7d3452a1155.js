4481649ad64d8a5fb881a86f1135fd8c04fb0032
Victor Godwin <victoranimate556@gmail.com>
2019-11-30 00:13:25 +0000

    Updates

---
+++ precache-manifest.50710d6939622180e813a7d3452a1155.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "29ace62a971bf49ad104",
+    "url": "/Social-App/static/css/main.67d71bcc.chunk.css"
+  },
+  {
+    "revision": "29ace62a971bf49ad104",
+    "url": "/Social-App/static/js/main.fa9a0c82.chunk.js"
+  },
+  {
+    "revision": "35fc73c9a56afcafd96e",
+    "url": "/Social-App/static/js/runtime~main.81645923.js"
+  },
+  {
+    "revision": "d84bbc00594edba7f2bb",
+    "url": "/Social-App/static/js/2.1fdac75c.chunk.js"
+  },
+  {
+    "revision": "6732bd426c60b47172b1c7296e886a60",
+    "url": "/Social-App/static/media/no-img.6732bd42.png"
+  },
+  {
+    "revision": "f4d3a6eac0ab28eddc6d512cd2c2eed6",
+    "url": "/Social-App/index.html"
+  }
+];
